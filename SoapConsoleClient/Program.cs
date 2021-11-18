91f56871f6a26181c601357b385594f9c88fa1de
chxtio <chxtio@gmail.com>
2021-11-17 19:44:10 -0800

    Add SOAP console client application

---
+++ SoapConsoleClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoapConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.Service1Client myClient = new ServiceReference1.Service1Client();
+            Console.WriteLine(myClient.add2(1, 3));
+            Console.ReadLine();
+        }
+    }
+}
